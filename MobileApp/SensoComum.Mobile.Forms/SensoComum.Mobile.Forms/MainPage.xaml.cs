9a7619a2fdcca064f86b9b4f45222535004ed4f2
Ricardo Serradas <Ricardo.Serradas@microsoft.com>
2018-07-24 15:28:05 -0300

    Adding first running version of the Android App

---
+++ MobileApp/SensoComum.Mobile.Forms/SensoComum.Mobile.Forms/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SensoComum.Mobile.Forms
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void OnSubjectView(object sender, EventArgs e)
+        {
+            int viewCount = 0;
+
+            int.TryParse(subjectViewCount.Text, out viewCount);
+
+            viewCount++;
+
+            subjectViewCount.Text = viewCount.ToString();
+        }
+    }
+}
