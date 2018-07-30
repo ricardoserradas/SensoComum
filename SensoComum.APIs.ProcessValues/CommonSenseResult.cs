bf5bed8ec0dd21f93082a383e0d71ba847ccaf03
Ricardo Serradas <Ricardo.Serradas@microsoft.com>
2018-07-30 11:30:32 -0300

    Creating the object in the table successfuly. Must fix the update

---
+++ SensoComum.APIs.ProcessValues/CommonSenseResult.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensoComum.APIs.ProcessValues
+{
+    public class CommonSenseResult : TableEntity
+    {
+        public CommonSenseResult(string name)
+        {
+            this.PartitionKey = name;
+            this.RowKey = name;
+        }
+
+        public CommonSenseResult()
+        {
+
+        }
+
+        public int Sum { get; set; }
+
+        public void Update(int value)
+        {
+            this.Sum += value;
+        }
+    }
+}
--- SensoComum.APIs.ProcessValues/ProcessQueue.cs
+++ SensoComum.APIs.ProcessValues/ProcessQueue.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage;
@@ -9,10 +10,41 @@ namespace SensoComum.APIs.ProcessValues
     public static class ProcessQueue
     {
         [FunctionName("ProcessQueue")]
-        public static void Run([QueueTrigger("values", Connection = "AzureWebJobsStorage")]string myQueueItem, TraceWriter log)
+        public static async Task Run([QueueTrigger("values", Connection = "AzureWebJobsStorage")]string myQueueItem, TraceWriter log)
         {
             int.TryParse(myQueueItem, out int value);
 
+            CloudTable table = await RetrieveCloudTableInstance();
+
+            CommonSenseResult commonSenseValue = await RetrieveCommonSenseValue(table);
+
+            commonSenseValue.Update(value);
+
+            await UpdateCommonSense(table, commonSenseValue);
+        }
+
+        private static async Task<CommonSenseResult> RetrieveCommonSenseValue(CloudTable table)
+        {
+            TableOperation retrieveValue = TableOperation.Retrieve<CommonSenseResult>("JeepCompass", "JeepCompass");
+
+            TableResult retrieveResult = await table.ExecuteAsync(retrieveValue);
+
+            CommonSenseResult commonSenseValue;
+
+            if (retrieveResult.Result == null)
+            {
+                commonSenseValue = await InitializeCommonSense(table);
+            }
+            else
+            {
+                commonSenseValue = (CommonSenseResult)retrieveResult.Result;
+            }
+
+            return commonSenseValue;
+        }
+
+        private static async Task<CloudTable> RetrieveCloudTableInstance()
+        {
             string connectionString = "DefaultEndpointsProtocol=https;AccountName=scapisstoragedev;AccountKey=RmYV4v06YsQJkUbs9Kp5L5WlwSKQo4LUI0XIV8q21nl3pfjHkoFuSYFz2zRV8CdrzhfqsJTtHlidr7wBDfeOyg==;EndpointSuffix=core.windows.net";
 
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
@@ -20,11 +52,34 @@ namespace SensoComum.APIs.ProcessValues
 
             CloudTable table = tableClient.GetTableReference("currentValue");
 
-            table.CreateIfNotExistsAsync();
+            bool tableExists = await table.CreateIfNotExistsAsync();
 
-            TableQuery query = new TableQuery().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "sum"));
+            if (!tableExists)
+            {
+                await table.CreateIfNotExistsAsync();
 
-            //var retrievedValue = table.
+                await InitializeCommonSense(table);
+            }
+
+            return table;
+        }
+
+        private static async Task UpdateCommonSense(CloudTable table, CommonSenseResult commonSenseValue)
+        {
+            TableOperation tableOperation = TableOperation.InsertOrReplace(commonSenseValue);
+
+            await table.ExecuteAsync(tableOperation);
+        }
+
+        private static async Task<CommonSenseResult> InitializeCommonSense(CloudTable table)
+        {
+            CommonSenseResult senseResult = new CommonSenseResult("JeepCompass");
+
+            TableOperation tableOperation = TableOperation.Insert(senseResult);
+
+            await table.ExecuteAsync(tableOperation);
+
+            return senseResult;
         }
     }
 }
